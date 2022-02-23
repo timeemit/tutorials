405c8390fa038798395cfdf6e25124b0d5334406
Liam Norris <liamnorris1231853211@gmail.com>
2022-02-23 17:16:10 +0000

    Reduce image size to 16x16

--- beginner_source/dcgan_faces_tutorial.py
+++ beginner_source/dcgan_faces_tutorial.py
@@ -186,13 +186,13 @@ batch_size = 128
 
 # Spatial size of training images. All images will be resized to this
 #   size using a transformer.
-image_size = 64
+image_size = 16  # @thoughtcodex / @timeemit -- Modified to reduce overall size of the model
 
 # Number of channels in the training images. For color images this is 3
 nc = 3
 
 # Size of z latent vector (i.e. size of generator input)
-nz = 1  # @thoughtcodex / @timeemit -- Modified from 100 to reduce overall size of the model
+nz = 100  # @thoughtcodex / @timeemit -- Modified from 100 to reduce overall size of the model
 
 # Size of feature maps in generator
 ngf = 64
@@ -336,15 +336,16 @@ class Generator(nn.Module):
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(ngf * 8),
-            nn.ReLU(True),
-            # state size. (ngf*8) x 4 x 4
-            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 4),
-            nn.ReLU(True),
-            # state size. (ngf*4) x 8 x 8
-            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(nz, ngf * 2, 4, 1, 0, bias=False),
+            # nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
+            # nn.BatchNorm2d(ngf * 8),
+            # nn.ReLU(True),
+            # # state size. (ngf*8) x 4 x 4
+            # nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            # nn.BatchNorm2d(ngf * 4),
+            # nn.ReLU(True),
+            # # state size. (ngf*4) x 8 x 8
+            # nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
@@ -416,16 +417,16 @@ class Discriminator(nn.Module):
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*2) x 16 x 16
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 4),
-            nn.LeakyReLU(0.2, inplace=True),
+            # # state size. (ndf*2) x 16 x 16
+            # nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            # nn.BatchNorm2d(ndf * 4),
+            # nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*4) x 8 x 8
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
-            nn.LeakyReLU(0.2, inplace=True),
+            # nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            # nn.BatchNorm2d(ndf * 8),
+            # nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Conv2d(ndf * 2, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
 
